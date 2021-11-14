cae0ad295ee7f2d9af85e897a2c0087ee90a293e
Alexey Kryuchkov <alexey.kryuchkov@jetbrains.com>
2021-11-14 17:17:14 +0300

    New symbols for inspection pattern, idea version up

--- src/main/java/ru/salamon/FailureInspectionFilter.java
+++ src/main/java/ru/salamon/FailureInspectionFilter.java
@@ -30,7 +30,7 @@ class FailureInspectionFilter implements Filter {
     private final Pattern PATTERN;
 
     public FailureInspectionFilter(Project project) {
-        PATTERN = Pattern.compile("[-+]\\s/([a-zA-Z0-9/._-]*):([0-9]*)\\s.*");
+        PATTERN = Pattern.compile("[-+]\\s/([()a-zA-Z0-9/._-]*):([0-9]*)\\s.*");
         myProject = project;
     }
 
